05ebd850e4a8191731d1af9a369e31650eae78fa
erick <erikc_48@hotmail.com>
2023-04-15 23:06:20 -0600

    Tutorial terminado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import Welcome from './components/Welcome';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Contact from "./pages/Contact";
+import AboutMe from './pages/AboutMe';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Erick Juarez Espinosa
-        </p>
-        <p>
-          De cierta manera la vida de un critico no es facil . . .
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to = "/">Home</Link>
+              </li>
+              <li>
+                <Link to = "/contact">Contacto</Link>
+              </li>
+              <li>
+                <Link to = "/about-me">About Me</Link>
+              </li>
+              <li>
+                <Link to = "/profile">Profile</Link>
+              </li>
+            </ul>
+          </div>
+          <Routes>
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about-me" element={<AboutMe />} />
+            <Route path="/profile/:name" element={<Profile />} />
+          </Routes>
+        </Router>
       </header>
     </div>
   );
